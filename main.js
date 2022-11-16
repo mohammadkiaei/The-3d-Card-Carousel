f4db0a4f8e6a78cee0817644f5b69c385751ab40
mohammadkiaei <s.kiaei.s.m.m.k1380@gmail.com>
2022-11-16 11:06:53 +0330

    Initial coomit

---
+++ main.js
@@ -0,0 +1,22 @@
+(function () {
+    var rotate, timeline;
+
+    rotate = function () {
+        return $('.card:first-child').fadeOut(400, 'swing', function () {
+            return $('.card:first-child').appendTo('.container').hide();
+        }).fadeIn(400, 'swing');
+    };
+
+    timeline = setInterval(rotate, 4000);
+
+    $('body').hover(function () {
+        return clearInterval(timeline);
+    });
+
+    $('.next').click(function () {
+        return rotate();
+    });
+    FPVHWCHD
+}).call(this);
+
+// Developed By SeyedMohammadMahdi Kiaei
